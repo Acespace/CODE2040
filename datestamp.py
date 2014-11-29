f34803acef7acd330e04d826d2b136e2014b1460
Acespace <alston.clark17@gmail.com>
2014-11-29 20:49:13 +0000

    Create datestamp.py
    Added file to send final date to server

---
+++ datestamp.py
@@ -0,0 +1,18 @@
+
+import requests
+import json
+import date_test
+from date_test import datestamp
+
+
+
+
+
+
+
+data = {'token':'hGO8rPbdnQ', 'datestamp': datestamp('85915058', '2007-05-22T12:18:00.000Z')}
+
+r = requests.post('http://challenge.code2040.org/api/validatetime', data=json.dumps(data))
+jsonData = r.json()
+
+print(jsonData)
